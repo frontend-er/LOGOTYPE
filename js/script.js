ae632fcf50647e23822f40f1f40c8ea9555e9a47
Dima <dbjenov@gmail.com>
2021-02-15 14:09:42 +0200

    Adaptive to menu 320px - done / burger - done

--- js/script.js
+++ js/script.js
@@ -159,3 +159,12 @@ DynamicAdapt.prototype.arraySort = function (arr) {
 
 const da = new DynamicAdapt("max");
 da.init();
+
+
+//Burger 
+$(document).ready(function() {
+	$('.menu__icon').click(function(event) {
+		$('.menu__icon,.menu__body').toggleClass('_active');
+		$('body').toggleClass('.lock');
+	});
+});
