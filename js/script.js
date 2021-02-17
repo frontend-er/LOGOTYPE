f18f33a23864a720c3eee471b96f7f70448fb10e
Dima <dbjenov@gmail.com>
2021-02-17 09:01:29 +0200

    sub menu - done => slider

--- js/script.js
+++ js/script.js
@@ -168,3 +168,27 @@ $(document).ready(function() {
 		$('body').toggleClass('.lock');
 	});
 });
+
+
+
+//menu - items - product
+
+let menuParents = document.querySelectorAll('.menu-page__parent');
+
+for (let index = 0; index < menuParents.length; index++) {
+	const menuParent = menuParents[index];
+	menuParent.addEventListener("mouseenter", function(e) {
+		menuParent.classList.add('_active');
+	});
+	menuParent.addEventListener("mouseleave", function(e) {
+		menuParent.classList.remove('_active');
+	});
+}
+
+let menuPageBurger = document.querySelector('.menu-page__burger');
+let  menuPageBody = document.querySelector('.menu-page__body');
+
+menuPageBurger.addEventListener("click", function(e) {
+	menuPageBurger.classList.toggle('_active');
+	menuPageBody.classList.toggle('_active');
+});
