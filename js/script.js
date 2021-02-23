569b44048230cda545942e721027db82de40f4fe
Dima <dbjenov@gmail.com>
2021-02-23 21:40:40 +0200

    Slider - stareted

--- js/script.js
+++ js/script.js
@@ -218,3 +218,70 @@ for (let index = 0; index < checkboxCategories.length; index++) {
 		}
 	});
 }
+
+
+
+//===================SLIDER ===================================================================================================//
+
+
+
+let sliders = document.querySelectorAll('._swiper');
+if (sliders) {
+	for (let index = 0; index < sliders.length; index++) {
+		let slider = sliders[index];
+		if(!slider.classList.contains('swiper-bild')){
+			let slider_items = slider.children;
+			if(slider_items) {
+				for (let index = 0; index < slider_items.length; index++ ) {
+					let el = slider_items[index];
+					el.classList.add('swiper-slide');
+				}
+			}
+			let slider_content = slider.innerHTML;
+			let slider_wrapper = document.createElement('div');
+			slider_wrapper.classList.add('swiper-wraper');
+			slider_wrapper.innerHTML = slider_content;
+			slider.innerHTML = '';
+			slider.appendChild(slider_wrapper);
+			slider.classList.add('swiper-bild');
+		}
+		if (slider.classList.contains('_gallery')) {
+			//slider.data('ligthGallery).destroy(true);
+		}
+	}
+	slider_bild_callback();
+}
+
+function slider_bild_callback(params) { };
+
+
+let mainslider = new Swiper ('.mainslider__body', {
+
+//},
+observer: true,
+observerParents: true,
+sliderPerView: 1,
+spaceBetween: 0, 
+autoHeight: true,
+speed: 800,
+
+pagination : {
+	el: '.mainslider__dots',
+	clikable: true,
+
+},
+
+/*
+navigation: {
+	nextEl: '.about__more .more__item_next',
+	prevEl: '.about__more .more__item_prev',
+},
+*/
+on: {
+	lazyImageReady: function () {
+		ibg();
+	},
+}
+
+
+});
